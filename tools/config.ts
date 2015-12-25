4aef7375f1214ed61b13f880a247793bf1026eb0
shootermv <shootermv@gmail.com>
2015-12-25 14:22:24 +0200

    font-awsome package added

--- tools/config.ts
+++ tools/config.ts
@@ -46,7 +46,7 @@ export const NPM_DEPENDENCIES = [
   { src: 'angular2/bundles/angular2.min.js', inject: 'libs', dest: LIB_DEST },
   { src: 'angular2/bundles/router.js', inject: 'libs', dest: LIB_DEST }, // use router.min.js with alpha47
   { src: 'angular2/bundles/http.min.js', inject: 'libs', dest: LIB_DEST },
-
+  { src: 'font-awesome/css/font-awesome.css', inject: true, dest: CSS_DEST },
   { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, dest: CSS_DEST }
 ];
 
