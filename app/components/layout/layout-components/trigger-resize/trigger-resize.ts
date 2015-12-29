0344127c1b5631c831315be7d4ddcdea8af470f8
shootermv <shootermv@gmail.com>
2015-12-29 08:40:05 +0200

    added trigger-resize compnent

---
+++ app/components/layout/layout-components/trigger-resize/trigger-resize.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: '[trigger-resize]',
+  templateUrl: './components/layout/layout-components/trigger-resize/trigger-resize.html'
+})
+export class TriggerResize {}
--- app/components/layout/top-navbar/top-navbar.ts
+++ app/components/layout/top-navbar/top-navbar.ts
@@ -1,11 +1,10 @@
 import {Component} from 'angular2/core';
-
-
-
+import {TriggerResize} from '../layout-components/trigger-resize/trigger-resize';
 
 @Component({
   selector: 'top-navbar',
-  templateUrl: './components/layout/top-navbar/top-navbar.html'
+  templateUrl: './components/layout/top-navbar/top-navbar.html',
+  directives: [TriggerResize]
 })
 export class TopNavbarCmp {
 }
