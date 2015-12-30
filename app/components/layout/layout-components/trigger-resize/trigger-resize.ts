0958dcb5e5c6ffd4fdd76a8acbe66d3fb34875fb
“wilmo02” <Moshe.Vilner@contractor.ca.com>
2015-12-30 14:04:08 +0200

    resize changes dody classes

--- app/components/layout/layout-components/trigger-resize/trigger-resize.ts
+++ app/components/layout/layout-components/trigger-resize/trigger-resize.ts
@@ -4,4 +4,9 @@ import {Component} from 'angular2/core';
   selector: '[trigger-resize]',
   templateUrl: './components/layout/layout-components/trigger-resize/trigger-resize.html'
 })
-export class TriggerResize {}
+export class TriggerResize {
+  onClickMe(event) {
+   event.preventDefault();
+   /aside-collapsed/.test(document.body.className) ? document.body.className = document.body.className.replace(' aside-collapsed','') :  document.body.className+=' aside-collapsed';
+  }
+}
