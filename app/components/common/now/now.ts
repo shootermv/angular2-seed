820f2bd0c6423ad2d599eafba3d088a68b5c4bf6
“wilmo02” <Moshe.Vilner@contractor.ca.com>
2016-01-06 12:04:55 +0200

    now directive added!!!

--- app/components/common/now/now.ts
+++ app/components/common/now/now.ts
@@ -1,13 +1,22 @@
-import {Component} from 'angular2/core';
-import {CORE_DIRECTIVES} from 'angular2/common';
+import {Component, Input, Attribute, ElementRef} from 'angular2/core'
 
 @Component({
   selector: 'now',
-  template:`koko`,
-  directives: [CORE_DIRECTIVES]
+  template: `
+      {{date | date: format}}
+    `
 })
 
 export class NowCmp {
-    
-    
+  private date;
+  private format;
+  constructor(@Attribute("format") format, myElement: ElementRef) { 
+    this.format = format;
+    this.date =  new Date(); 
+    let that= this;
+    setInterval(() => {
+        that.date =  new Date();
+     }, 1000);
+  } 
+
 }
--- app/components/dashboard/dashboard.ts
+++ app/components/dashboard/dashboard.ts
@@ -1,11 +1,11 @@
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES} from 'angular2/common';
-
+import {NowCmp} from '../common/now/now';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './components/dashboard/dashboard.html',
   styleUrls: ['./components/dashboard/dashboard.css'],
-  directives: [CORE_DIRECTIVES]
+  directives: [CORE_DIRECTIVES, NowCmp]
 })
 export class DashboardCmp {}
