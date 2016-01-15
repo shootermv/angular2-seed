e5b62a554526e1a1a6f1a3f05a159155b84c50c6
“wilmo02” <Moshe.Vilner@contractor.ca.com>
2016-01-15 11:21:47 +0200

    now directive - new way

--- app/components/common/now/now.ts
+++ app/components/common/now/now.ts
@@ -1,17 +1,20 @@
-import {Component, Attribute, ElementRef} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 
 @Component({
   selector: 'now',
   template: `
-      {{date | date: format}}
+      {{date | date: _format}}
     `
 })
 
 export class NowCmp {
   private date;
-  private format;
-  constructor(@Attribute('format') format, myElement: ElementRef) {
-    this.format = format;
+  
+  @Input() set format(formatName:string){
+     this._format = formatName;
+   
+  }
+  constructor() {    
     this.date =  new Date();
     setInterval(() => {
         this.date =  new Date();
