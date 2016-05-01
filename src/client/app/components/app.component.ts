5d5c352f43424a8d3119f76dbf08fa0ed96253a3
shootermv <shootermv@gmail.com>
2016-05-01 23:08:42 +0300

    home & about components removed

--- src/client/app/components/app.component.ts
+++ src/client/app/components/app.component.ts
@@ -3,8 +3,6 @@ import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {NavbarComponent} from './navbar.component';
 import {ToolbarComponent} from './toolbar.component';
 import {NameListService} from '../shared/index';
-import {HomeComponent} from '../+home/index';
-import {AboutComponent} from '../+about/index';
 import {TopNavbarCmp} from './top-navbar/top-navbar';
 import {SideBarCmp} from './sidebar/sidebar';
 import {WidgetsCmp} from '../widgets/widgets';
