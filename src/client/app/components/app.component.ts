4c78edfbc657889575d265555f1be3e70700cc23
shootermv <shootermv@gmail.com>
2016-05-08 16:31:17 +0300

    removing no more needed components

--- src/client/app/components/app.component.ts
+++ src/client/app/components/app.component.ts
@@ -1,8 +1,6 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {NameListService} from '../shared/index';
-import {TopNavbarCmp} from './top-navbar/top-navbar';
-import {SideBarCmp} from './sidebar/sidebar';
 import {WidgetsCmp} from '../widgets/widgets';
 import {DashboardCmp} from '../dashboard/dashboard';
 import {Preloader} from '../common/preloader/preloader';
@@ -13,7 +11,7 @@ import {PagesComponent} from '../pages/index';
   selector: 'sd-app',
   viewProviders: [NameListService],
   templateUrl: 'app/components/app.component.html',
-  directives: [ROUTER_DIRECTIVES, Preloader, SideBarCmp, TopNavbarCmp, WidgetsCmp, DashboardCmp]
+  directives: [ROUTER_DIRECTIVES, Preloader, WidgetsCmp, DashboardCmp]
 })
 @RouteConfig([
   {
