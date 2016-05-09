b56901e552f6253998bf6548fb65610b1b5d8835
shootermv <shootermv@gmail.com>
2016-05-09 11:11:59 +0300

    extra files removed from "layout" dir too

--- src/client/app/layout/components/toolbar.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-  selector: 'sd-toolbar',
-  templateUrl: 'app/layout/components/toolbar.component.html',
-  styleUrls: ['app/layout/components/toolbar.component.css']
-})
-export class ToolbarComponent {}
