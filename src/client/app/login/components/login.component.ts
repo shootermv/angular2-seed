2caaa5b48fa6c39794172ec997a2fc70b3c30a4d
shootermv <shootermv@gmail.com>
2016-05-15 15:29:27 +0300

    login handler

--- src/client/app/login/components/login.component.ts
+++ src/client/app/login/components/login.component.ts
@@ -1,7 +1,12 @@
 import {Component} from 'angular2/core';
-
+import {ComponentInstruction, Router, Location, OnActivate} from 'angular2/router';
 @Component({
   templateUrl: 'app/login/components/login.component.html',
   styleUrls: ['app/login/components/login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(public router: Router) {}
+  login() {
+   this.router.navigateByUrl('/layout');
+  }
+}
